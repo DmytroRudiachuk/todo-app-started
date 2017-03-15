4f68f5f81c1f4e9e5ec945ecf799332e4f0d140f
keri19372 <keri19372@gmail.com>
2017-03-15 11:04:58 +0200

    added css

--- javascripts/index.js
+++ javascripts/index.js
@@ -1,4 +1,5 @@
 var inputText = document.querySelector("#todoText");
+var todosList = document.querySelector("#todoList");
 var todos = [];
 
 inputText.onkeypress = function(e) {
@@ -6,12 +7,20 @@ inputText.onkeypress = function(e) {
 		todos.push({
 			text: inputText.value,
 			isDone: false
-		});
-
+		});		
 		inputText.value = "";
+		renderTodos();
 	}
 }
 
 function renderTodos() {
-	
+	var todoElementTemplete = document.querySelector("div li").cloneNode(true);
+
+	todos.forEach(function(todo){
+		todoElementTemplete.querySelector("span").innerText = todo.text;
+		todoElementTemplete.querySelector("input").onchange = function(e) {
+			console.log(e);
+		}
+		todosList.appendChild(todoElementTemplete);
+	});
 }
