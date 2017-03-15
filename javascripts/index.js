0608849e4c1004422f71c60d23c8120bb82b9e52
keri19372 <keri19372@gmail.com>
2017-03-15 14:09:47 +0200

    done todos

--- javascripts/index.js
+++ javascripts/index.js
@@ -1,5 +1,6 @@
 var inputText = document.querySelector("#todoText");
 var todosList = document.querySelector("#todoList");
+var todosLeft = document.querySelector("#todosLeft");
 var todos = [];
 
 inputText.onkeypress = function(e) {
@@ -16,11 +17,33 @@ inputText.onkeypress = function(e) {
 function renderTodos() {
 	var todoElementTemplete = document.querySelector("div li").cloneNode(true);
 
-	todos.forEach(function(todo){
+	todos.forEach(function(todo, i){
 		todoElementTemplete.querySelector("span").innerText = todo.text;
+		todoElementTemplete.setAttribute("todo-index", i);
 		todoElementTemplete.querySelector("input").onchange = function(e) {
-			console.log(e);
+			var li = e.path[1];
+			var todoIndex = li.getAttribute("todo-index");
+			var todo = todos[todoIndex];
+
+			if(e.path[0].checked) {		
+
+				li.setAttribute("class","todo-done");
+				todo.isDone = true;
+			} else {
+				li.setAttribute("class","");
+				todo.isDone = false;
+
+			}
 		}
 		todosList.appendChild(todoElementTemplete);
 	});
+}
+
+function countActiveTodos() {
+	var activeTodos = todos.filter(function(todo){
+		return todo.isDone == false;
+	})
+
+	todosLeft.innerText = activeTodos.length;
+
 }
