98b3a464c8a89d0b553e80c3565d39cfda5bba53
keri19372 <keri19372@gmail.com>
2017-03-18 12:34:23 +0200

    Changed index.js

--- javascripts/index.js
+++ javascripts/index.js
@@ -1,49 +1,123 @@
 var inputText = document.querySelector("#todoText");
 var todosList = document.querySelector("#todoList");
 var todosLeft = document.querySelector("#todosLeft");
-var todos = [];
+var clearCompleted = document.querySelector("#clearCompleted");
+var markAllCompleted = document.querySelector("#markAllCompleted");
+var todoIndexValue = 0;
+
+var todos = [{
+    text: "first todo",
+    isDone: false,
+    index: 0
+}];
 
 inputText.onkeypress = function(e) {
-	if(e.keyCode == 13) {
-		todos.push({
-			text: inputText.value,
-			isDone: false
-		});		
-		inputText.value = "";
-		renderTodos();
-	}
+    if (e.keyCode == 13) {
+        todoIndexValue++;
+        todos.push({
+            text: inputText.value,
+            isDone: false,
+            index: todoIndexValue
+        });
+        inputText.value = "";
+        renderTodos();
+        countActiveTodos();
+    }
+}
+
+clearCompleted.onclick = function() {
+    todos.forEach(function(todo, i) {
+        if (todo.isDone == true) {
+            var li = document.querySelector("li[todo-index='" + todo.index + "']");
+            todos.splice(i, 1);
+            // TODO: investigate todo.index possible workflow
+            todosList.removeChild(li);
+        }
+    });
+}
+
+markAllCompleted.onclick = function() {
+    var activeTodos = todos.filter(function(todo) {
+        return todo.isDone == false;
+    }).length;
+
+    if (activeTodos == 0) {
+        todos.forEach(function(todo) {
+            var li = document.querySelector("li[todo-index='" + todo.index + "']");
+            var checkbox = li.querySelector("input");
+
+            todo.isDone = false;
+            checkbox.checked = false;
+            li.setAttribute("class", "")
+        });
+    } else {
+        todos.forEach(function(todo) {
+            var li = document.querySelector("li[todo-index='" + todo.index + "']");
+            var checkbox = li.querySelector("input");
+
+            todo.isDone = true;
+            checkbox.checked = true;
+            li.setAttribute("class", "todo-done")
+        });
+    }
+
+    countActiveTodos();
 }
 
 function renderTodos() {
-	var todoElementTemplete = document.querySelector("div li").cloneNode(true);
+    var todoElementTemplate = document.querySelector("div li").cloneNode(true);
 
-	todos.forEach(function(todo, i){
-		todoElementTemplete.querySelector("span").innerText = todo.text;
-		todoElementTemplete.setAttribute("todo-index", i);
-		todoElementTemplete.querySelector("input").onchange = function(e) {
-			var li = e.path[1];
-			var todoIndex = li.getAttribute("todo-index");
-			var todo = todos[todoIndex];
+    if (todos.length == 0) {
+        todosList.innerHTML = "";
+        return;
+    }
 
-			if(e.path[0].checked) {		
+    todos.forEach(function(todo) {
+        todoElementTemplate.querySelector("span").innerText = todo.text;
+        todoElementTemplate.setAttribute("todo-index", todo.index)
+        todoElementTemplate.querySelector("input").onchange = function(e) {
+            var li = e.path[1];
+            var todoIndex = li.getAttribute("todo-index");
+            var todo = todos.filter(function(todo) {
+                return todo.index == todoIndex;
+            });
 
-				li.setAttribute("class","todo-done");
-				todo.isDone = true;
-			} else {
-				li.setAttribute("class","");
-				todo.isDone = false;
+            todo = todos.indexOf(todo[0]);
+            todo = todos[todo];
 
-			}
-		}
-		todosList.appendChild(todoElementTemplete);
-	});
+            if (e.path[0].checked) {
+                li.setAttribute("class", "todo-done");
+                todo.isDone = true;
+            } else {
+                li.setAttribute("class", "");
+                todo.isDone = false;
+            }
+            countActiveTodos();
+        }
+        todoElementTemplate.querySelector("button").onclick = function(e) {
+            var li = e.path[1];
+            var todoIndex = li.getAttribute("todo-index");
+            var todo = todos.filter(function(todo) {
+                return todo.index == todoIndex;
+            });
+
+            todoIndex = todos.indexOf(todo[0]);
+            todos.splice(Number(todoIndex), 1);
+
+            todosList.removeChild(li);
+            countActiveTodos();
+        }
+        todosList.appendChild(todoElementTemplate);
+    });
 }
 
 function countActiveTodos() {
-	var activeTodos = todos.filter(function(todo){
-		return todo.isDone == false;
-	})
+    var activeTodos = todos.filter(function(todo) {
+        return todo.isDone == false;
+    });
 
-	todosLeft.innerText = activeTodos.length;
-
+    todosLeft.innerText = activeTodos.length;
 }
+
+renderTodos();
+countActiveTodos();
